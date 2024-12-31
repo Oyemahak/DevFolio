b53635248073b35f32ab9a355c81031f42e2a9e1
Your NameMahak <mahakpateluiux@gmail.com>
2024-12-30 23:43:03 -0500

    Initial commit: Angular project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, HomeComponent],
+  template: `
+    <app-header></app-header>
+    <app-home></app-home>
+    <app-footer></app-footer>
+  `,
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DevFolio';
-}
+export class AppComponent {}
